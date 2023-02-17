ef9c1a8f3eb96396f4e9b6972201c391312ef259
Joon_Thinkpad <mjypark1212@gmail.com>
2023-02-17 15:52:10 +0800

    Error Color pallete

--- frontend/lib/constants/colors.dart
+++ frontend/lib/constants/colors.dart
@@ -7,7 +7,7 @@ Color neutralColor = const Color(0xff6b7280);
 Color infoColor = const Color(0xffe0f2fe);
 Color successColor = const Color(0xff36D399);
 Color warningColor = const Color(0xfffbbd23);
-Color errorColor = const Color(0xf43f5e);
+Color errorColor = const Color(0xfff43f5e);
 
 Color titleTextColor = Colors.grey.shade900;
 Color contentTextColor = Colors.grey.shade600;
