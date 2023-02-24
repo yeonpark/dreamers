8654526586db99e811067997f53c8051717cfa84
Joon_Thinkpad <mjypark1212@gmail.com>
2023-02-24 17:27:55 +0800

    Coinbase & routes updated

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:dreamers/pages/main_page.dart';
+import 'package:dreamers/pages/search_page.dart';
 import 'package:flutter/material.dart';
 import 'package:dreamers/pages/feed.dart';
 import 'package:dreamers/pages/itemlist.dart';
+import 'package:dreamers/pages/connect_coinbase_page.dart';
 
 //import '../test_data/success_info.dart';
 //import '../test_data/success_info.dart';
@@ -43,59 +46,16 @@ class _MyAppState extends State<MyApp> {
         ),
       ),
     ),
-    Center(
-      child: Text(
-        "where menu 4 should be",
-        style: TextStyle(
-          fontSize: 40,
-        ),
-      ),
-    ),
+    ConnectWalletPage()
   ];
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          /*
-      This Scaffold is consisted only with
-      body and bottomNavigationBar.
-      Body displays the screens based on user's menu selection.
-      Default screen (when user first open this app) is first menu page.
-      BottomNavigationBar is for menu selection. If user clicks second menu,
-      currentIndex changes to 1, so body will display second page (Dart's zero-based indexing.
-      */
-          body: screens[currentIndex],
-          bottomNavigationBar: BottomNavigationBar(
-            currentIndex: currentIndex,
-            onTap: (index) => setState(() => currentIndex = index),
-            type: BottomNavigationBarType.fixed,
-            backgroundColor: Colors.white,
-            selectedItemColor: Color(0xffff295d),
-            unselectedItemColor: Colors.black.withOpacity(0.5),
-            items: [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.search),
-                label: 'Menu 1',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.favorite_border_outlined),
-                label: 'Menu 2',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.logo_dev),
-                label: 'Dreamers',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.textsms_outlined),
-                label: 'Menu 3',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.portrait),
-                label: 'Menu 4',
-              ),
-            ],
-          )),
-    );
+    return MaterialApp(initialRoute: "/main", routes: {
+      "/main": (context) => const MainPage(),
+      "/connect-wallet": (context) => const ConnectWalletPage(),
+      "/search": (context) => const SearchPage(),
+      "/success-story": (context) => const FeedPage(),
+    });
   }
 }
